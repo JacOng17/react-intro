15b2396ba13de026844836e572b6767f926bdc23
JacOng17 <jacob_ong88@hotmail.com>
2019-02-18 14:55:00 +1300

    Updates

---
+++ precache-manifest.0ec6ed6338c398ce35ef5155e9606503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcaa670e47d6cb15753e",
+    "url": "/react-intro/static/css/main.c4cc0bc1.chunk.css"
+  },
+  {
+    "revision": "dcaa670e47d6cb15753e",
+    "url": "/react-intro/static/js/main.dcaa670e.chunk.js"
+  },
+  {
+    "revision": "58ae9d67131fa03c3da8",
+    "url": "/react-intro/static/js/runtime~main.58ae9d67.js"
+  },
+  {
+    "revision": "49705b0256a22830c507",
+    "url": "/react-intro/static/js/2.49705b02.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-intro/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d49a956318ba8e63e7eff1c0f69947d1",
+    "url": "/react-intro/index.html"
+  }
+];
