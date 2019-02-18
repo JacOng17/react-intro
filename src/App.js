d1ed7753b4b73f14969dccedcba48e2ba62bb020
JacOng17 <jacob_ong88@hotmail.com>
2019-02-18 14:35:44 +1300

    Added clock component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greetings from "./components/Greetings";
 import SimpleForm from './components/SimpleForm';
+import Clock from './components/clock.js'
 
 const App = () => (
   <div className="App">
@@ -10,10 +12,9 @@ const App = () => (
       <img src={logo} className="App-logo" alt="logo" />
       <Greetings firstName="John" lastName="Smith" />
     </header>
-    <div>
     
-  </div>
     <SimpleForm />
+    <Clock />
   </div>
 );
 
