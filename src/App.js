d827de8a32dffa362298923ed69fbdcb88f78c88
JacOng17 <jacob_ong88@hotmail.com>
2019-02-19 15:56:46 +1300

    Added rows to practice.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,40 @@ import React from 'react';
 // import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greetings from "./components/Greetings";
+// import Greetings from "./components/Greetings";
 import SimpleForm from './components/SimpleForm';
-import Clock from './components/clock.js'
+import Clock from './components/clock.js';
+import { Container, Row, Col } from 'react-grid-system'
 
 // Where you add your components (i.e. SimpleForm, Clock, etc...)
 const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <Greetings firstName="Jacob" lastName="Ong" />
+      {/* <Greetings firstName="Jacob" lastName="Ong" /> */}
     </header>
-    
-    <SimpleForm />
+    <Container>
+  <Row>
+    <Col sm={4}>
+      <SimpleForm />
+    </Col>
+    <Col sm={4}>
+      <Clock />
+    </Col>
+    <Col sm={4}>
+      One of three columns
+    </Col>
+  </Row>
+  <Row>
+    <Col md={6}>
     <Clock />
+    </Col>
+    <Col md={6}>
+    One of two columns
+    </Col>
+  </Row>
+</Container>
+
   </div>
 );
 
